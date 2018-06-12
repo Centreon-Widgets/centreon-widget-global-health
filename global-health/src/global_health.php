b2d678afbec620998b12d989c99cd66c6d0dd8ef
v.radev <v.radev@live.com>
2018-06-12 17:18:21 +0300

    fix(filtering): fix formatting and variable names

--- global-health/src/global_health.php
+++ global-health/src/global_health.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -33,8 +33,7 @@
  *
  */
 
-require_once "../../require.php";
-
+require_once '../../require.php';
 require_once $centreon_path . 'www/class/centreon.class.php';
 require_once $centreon_path . 'www/class/centreonSession.class.php';
 require_once $centreon_path . 'www/class/centreonDB.class.php';
@@ -54,10 +53,12 @@ if (!isset($_SESSION['centreon']) || !isset($_REQUEST['widgetId'])) {
 
 $db = new CentreonDB();
 $pearDB = $db;
+
 if (CentreonSession::checkSession(session_id(), $db) == 0) {
     exit;
 }
-$dbb = new CentreonDB("centstorage");
+
+$dbb = new CentreonDB('centstorage');
 
 /* Init Objects */
 $criticality = new CentreonCriticality($db);
@@ -66,21 +67,18 @@ $media = new CentreonMedia($db);
 /**
  * Displaying a Smarty Template
  */
-$path = $centreon_path . "www/widgets/global-health/src/";
+$path = $centreon_path . 'www/widgets/global-health/src/';
 $template = new Smarty();
-$template = initSmartyTplForPopup($path, $template, "./", $centreon_path);
-$template->assign("session", session_id());
-$template->assign("host_label", _("Hosts"));
-$template->assign("svc_label", _("Services"));
+$template = initSmartyTplForPopup($path, $template, './', $centreon_path);
+$template->assign('session', session_id());
+$template->assign('host_label', _('Hosts'));
+$template->assign('svc_label', _('Services'));
 
 $centreon = $_SESSION['centreon'];
 $widgetId = $_REQUEST['widgetId'];
-
 $widgetObj = new CentreonWidget($centreon, $db);
 $preferences = $widgetObj->getWidgetPreferences($widgetId);
-
-
-$oreon = $_SESSION["centreon"];
+$oreon = $_SESSION['centreon'];
 
 /**
  * Initiate Language class
@@ -88,181 +86,180 @@ $oreon = $_SESSION["centreon"];
 $centreonLang = new CentreonLang($centreon_path, $oreon);
 $centreonLang->bindLang();
 
-
 /**
  * Tab status
  */
-$tabSatusHost 		= array(0 => "UP", 1 => "DOWN", 2 => "UNREACHABLE", 4 => "PENDING");
-$tabSatusService 	= array(0 => "OK", 1 => "WARNING", 2 => "CRITICAL", 3 => "UNKNOWN", 4 => "PENDING");
+$tabStatusHost = [0 => 'UP', 1 => 'DOWN', 2 => 'UNREACHABLE', 4 => 'PENDING'];
+$tabStatusService = [0 => 'OK', 1 => 'WARNING', 2 => 'CRITICAL', 3 => 'UNKNOWN', 4 => 'PENDING'];
+$serviceArray = [];
+$hostArray = [];
 
-$serviceArray = array();
-$hostArray = array();
-
-foreach($tabSatusService as $key=>$statusService){
-    $serviceArray[$tabSatusService[$key]]['value'] = 0;
-    $serviceArray[$tabSatusService[$key]]['acknowledged'] = 0;
-    $serviceArray[$tabSatusService[$key]]['downtime'] = 0;
-    $serviceArray[$tabSatusService[$key]]['percent'] = 0;
+foreach($tabStatusService as $key=>$statusService){
+    $serviceArray[$tabStatusService[$key]]['value'] = 0;
+    $serviceArray[$tabStatusService[$key]]['acknowledged'] = 0;
+    $serviceArray[$tabStatusService[$key]]['downtime'] = 0;
+    $serviceArray[$tabStatusService[$key]]['percent'] = 0;
 }
 
-foreach($tabSatusHost as $key=>$statusHost){
-    $hostArray[$tabSatusHost[$key]]['value'] = 0;
-    $hostArray[$tabSatusHost[$key]]['acknowledged'] = 0;
-    $hostArray[$tabSatusHost[$key]]['downtime'] = 0;
-    $hostArray[$tabSatusHost[$key]]['percent'] = 0;
+foreach($tabStatusHost as $key=>$statusHost){
+    $hostArray[$tabStatusHost[$key]]['value'] = 0;
+    $hostArray[$tabStatusHost[$key]]['acknowledged'] = 0;
+    $hostArray[$tabStatusHost[$key]]['downtime'] = 0;
+    $hostArray[$tabStatusHost[$key]]['percent'] = 0;
 }
 
 if(isset($preferences['hosts_services']) && $preferences['hosts_services'] == 'hosts'){
-        
-    $hgName = false;
-    $innerjoingroup = '';
-    $wheregroup = '';
+    $hostGroupName = false;
+    $innerJoinGroup = '';
 
     if(!empty($preferences['hostgroup'])){
-        $sql = 'select hg.hg_name from hostgroup hg where hg.hg_id = ' . $dbb->escape($preferences['hostgroup'])."";
-        $DBRESULT = $db->query($sql);
-        $row = $DBRESULT->fetchRow();
-        $hgName = $row['hg_name'];
+        $sql = 'select hg.hg_name from hostgroup hg where hg.hg_id = ' . $dbb->escape($preferences['hostgroup']);
+        $dbResult = $db->query($sql);
+        $row = $dbResult->fetchRow();
+        $hostGroupName = $row['hg_name'];
     }
 
-    if($hgName){
-        $innerjoingroup = ' INNER JOIN hosts_hostgroups hhg ON  h.host_id = hhg.host_id ' .
-                           " INNER JOIN hostgroups hg ON hhg.hostgroup_id = hg.hostgroup_id and hg.name = '".$hgName."' ";
+    if($hostGroupName){
+        $innerJoinGroup = ' INNER JOIN hosts_hostgroups hhg ON  h.host_id = hhg.host_id ' .
+                           ' INNER JOIN hostgroups hg ON hhg.hostgroup_id = hg.hostgroup_id and hg.name = \'' . $hostGroupName . '\' ';
     }
-    
+
 	/**$obj->DBC->escape($instance)
 	 * Get DB informations for creating Flash
 	 */
-	$rq1 = 	" SELECT count(DISTINCT h.name) cnt, h.state, SUM(h.acknowledged) as acknowledged, SUM(CASE WHEN h.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  " .
-			" FROM `hosts` h " .
-			$innerjoingroup .
-			" WHERE h.enabled = 1 " .
-			$oreon->user->access->queryBuilder("AND", "h.name", $oreon->user->access->getHostsString("NAME", $dbb)) .
-			" AND h.name NOT LIKE '_Module_%' " .
-			" GROUP BY h.state " .
-			" ORDER BY h.state";
-	$DBRESULT = $dbb->query($rq1);
-	$data = array();
-	$color = array();
-	$legend = array();
+	$rq1 = ' SELECT count(DISTINCT h.name) cnt, h.state, SUM(h.acknowledged) as acknowledged, SUM(CASE WHEN h.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  ' .
+			' FROM `hosts` h ' .
+			$innerJoinGroup .
+			' WHERE h.enabled = 1 ' .
+			$oreon->user->access->queryBuilder('AND', 'h.name', $oreon->user->access->getHostsString('NAME', $dbb)) .
+			' AND h.name NOT LIKE \'_Module_%\' ' .
+			' GROUP BY h.state ' .
+			' ORDER BY h.state';
+	$dbResult = $dbb->query($rq1);
+	$data = [];
+	$color = [];
+	$legend = [];
 	$counter = 0;
-	while ($ndo = $DBRESULT->fetchRow()){
-        $data[$ndo["state"]]['count'] = $ndo["cnt"];
-        $data[$ndo["state"]]['acknowledged'] = $ndo["acknowledged"];
-        $data[$ndo["state"]]['downtime'] = $ndo["downtime"];
-		//$data[] = $ndo["cnt"];
-		//$legend[] = $tabSatusHost[$ndo["state"]];
-		//$color[] = $oreon->optGen["color_".strtolower($tabSatusHost[$ndo["state"]])];
-		$counter += $ndo["cnt"];
+
+	while ($ndo = $dbResult->fetchRow()){
+        $data[$ndo['state']]['count'] = $ndo['cnt'];
+        $data[$ndo['state']]['acknowledged'] = $ndo['acknowledged'];
+        $data[$ndo['state']]['downtime'] = $ndo['downtime'];
+		//$data[] = $ndo['cnt'];
+		//$legend[] = $tabStatusHost[$ndo['state']];
+		//$color[] = $oreon->optGen['color_'.strtolower($tabStatusHost[$ndo['state']])];
+		$counter += $ndo['cnt'];
 	}
-	$DBRESULT->free();
+
+	$dbResult->free();
 
 	foreach ($data as $key => $value) {
-        $hostArray[$tabSatusHost[$key]]['value'] = $value['count'];
+        $hostArray[$tabStatusHost[$key]]['value'] = $value['count'];
 		$valuePercent = round($value['count'] / $counter * 100, 2);
-	  	$valuePercent = str_replace(",", ".", $valuePercent);
-	  	$hostArray[$tabSatusHost[$key]]['percent'] = $valuePercent;
-        $hostArray[$tabSatusHost[$key]]['acknowledged'] = $value['acknowledged'];
-        $hostArray[$tabSatusHost[$key]]['downtime'] = $value['downtime'];
-        //$hostArray[$tabSatusHost[$key]]['color'] = $oreon->optGen["color_".strtolower($tabSatusHost[$key])];
+	  	$valuePercent = str_replace(',', '.', $valuePercent);
+	  	$hostArray[$tabStatusHost[$key]]['percent'] = $valuePercent;
+        $hostArray[$tabStatusHost[$key]]['acknowledged'] = $value['acknowledged'];
+        $hostArray[$tabStatusHost[$key]]['downtime'] = $value['downtime'];
+        //$hostArray[$tabStatusHost[$key]]['color'] = $oreon->optGen['color_'.strtolower($tabStatusHost[$key])];
 	}
-    
-    $template->assign("hosts", $hostArray);
-    $template->display("global_health_host.ihtml");
 
-}else if(isset($preferences['hosts_services']) && $preferences['hosts_services'] == "services"){
+    $template->assign('hosts', $hostArray);
+    $template->display('global_health_host.ihtml');
 
-    $sgName = false;
-    $hgName = false;
-    $innerjoingroup = '';
+}else if(isset($preferences['hosts_services']) && $preferences['hosts_services'] == 'services'){
+	global $is_admin;
+
+    $serviceGroupName = false;
+    $hostGroupName = false;
+    $innerJoinGroup = '';
 
     if(!empty($preferences['servicegroup'])){
-        $sql = "select sg.sg_name from servicegroup sg where sg.sg_id = ".$dbb->escape($preferences['servicegroup'])."";
-        $DBRESULT = $db->query($sql);
-        $row = $DBRESULT->fetchRow();
-        $sgName = $row['sg_name'];
+        $sql = 'select sg.sg_name from servicegroup sg where sg.sg_id = ' . $dbb->escape($preferences['servicegroup']);
+        $dbResult = $db->query($sql);
+        $row = $dbResult->fetchRow();
+        $serviceGroupName = $row['sg_name'];
     }
 
     if(!empty($preferences['hostgroup'])){
-        $sql = "select hg.hg_name from hostgroup hg where hg.hg_id = ".$dbb->escape($preferences['hostgroup'])."";
-        $DBRESULT = $db->query($sql);
-        $row = $DBRESULT->fetchRow();
-        $hgName = $row['hg_name'];
+        $sql = 'select hg.hg_name from hostgroup hg where hg.hg_id = ' . $dbb->escape($preferences['hostgroup']);
+        $dbResult = $db->query($sql);
+        $row = $dbResult->fetchRow();
+        $hostGroupName = $row['hg_name'];
     }
 
-    if($sgName){
-        $innerjoingroup = ' INNER JOIN services_servicegroups ssg ON ssg.service_id = s.service_id and ssg.host_id = s.host_id ' .
-                           " INNER JOIN servicegroups sg ON ssg.servicegroup_id = sg.servicegroup_id and sg.name = '".$sgName."' ";
+    if($serviceGroupName){
+        $innerJoinGroup = ' INNER JOIN services_servicegroups ssg ON ssg.service_id = s.service_id and ssg.host_id = s.host_id ' .
+                           ' INNER JOIN servicegroups sg ON ssg.servicegroup_id = sg.servicegroup_id and sg.name = \'' . $serviceGroupName . '\' ';
     }
 
-    if($hgName){
-        $innerjoingroup .= ' INNER JOIN hosts_hostgroups hhg ON  h.host_id = hhg.host_id ' .
-            " INNER JOIN hostgroups hg ON hhg.hostgroup_id = hg.hostgroup_id and hg.name = '".$hgName."' ";
+    if($hostGroupName){
+        $innerJoinGroup .= ' INNER JOIN hosts_hostgroups hhg ON  h.host_id = hhg.host_id ' .
+            ' INNER JOIN hostgroups hg ON hhg.hostgroup_id = hg.hostgroup_id and hg.name = \'' . $hostGroupName . '\' ';
     }
-    
-	global $is_admin;
 
 	$is_admin =  $oreon->user->admin;
-	$grouplistStr = $oreon->user->access->getAccessGroupsString();
+	$groupListString = $oreon->user->access->getAccessGroupsString();
 
 	/**
-	 * Get DB informations for creating Flash
+	 * Get DB information for creating Flash
 	 */
-	if (!$is_admin) {
-		$rq2 = 	" SELECT count(DISTINCT s.state, s.host_id, s.service_id) count, s.state state, " .
-				" SUM(s.acknowledged) as acknowledged, SUM(CASE WHEN s.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  " .
-				" FROM services s " .
-				" INNER JOIN centreon_acl acl ON s.host_id  = acl.host_id AND s.service_id = acl.service_id  " .
-				" INNER JOIN hosts h ON s.host_id = h.host_id " .
-				$innerjoingroup .
-				" WHERE h.name NOT LIKE '_Module_%' ".
-				" AND h.enabled = 1 " .
-				" AND s.enabled = 1 " .
-				" AND acl.group_id IN (".$grouplistStr.") ".
-				" GROUP BY s.state ORDER by s.state";
+	if ($is_admin) {
+        $rq2 = 	' SELECT count(DISTINCT s.state, s.host_id, s.service_id) count, s.state state, ' .
+            ' SUM(s.acknowledged) as acknowledged, SUM(CASE WHEN s.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  ' .
+            ' FROM services s' .
+            ' INNER JOIN hosts h ON s.host_id = h.host_id ' .
+            $innerJoinGroup .
+            ' WHERE h.name NOT LIKE \'_Module_%\' '.
+            ' AND h.enabled = 1 '.
+            ' AND s.enabled = 1 '.
+            ' GROUP BY s.state ORDER by s.state';
 	} else {
-		$rq2 = 	" SELECT count(DISTINCT s.state, s.host_id, s.service_id) count, s.state state, " .
-				" SUM(s.acknowledged) as acknowledged, SUM(CASE WHEN s.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  " .
-				" FROM services s" .
-				" INNER JOIN hosts h ON s.host_id = h.host_id " .
-				$innerjoingroup .
-				" WHERE h.name NOT LIKE '_Module_%' ".
-				" AND h.enabled = 1 ".
-				" AND s.enabled = 1 ".
-				" GROUP BY s.state ORDER by s.state";
+        $rq2 = 	' SELECT count(DISTINCT s.state, s.host_id, s.service_id) count, s.state state, ' .
+            ' SUM(s.acknowledged) as acknowledged, SUM(CASE WHEN s.scheduled_downtime_depth >= 1 THEN 1 ELSE 0 END) AS downtime  ' .
+            ' FROM services s ' .
+            ' INNER JOIN centreon_acl acl ON s.host_id  = acl.host_id AND s.service_id = acl.service_id  ' .
+            ' INNER JOIN hosts h ON s.host_id = h.host_id ' .
+            $innerJoinGroup .
+            ' WHERE h.name NOT LIKE \'_Module_%\' '.
+            ' AND h.enabled = 1 ' .
+            ' AND s.enabled = 1 ' .
+            ' AND acl.group_id IN (' . $groupListString . ') '.
+            ' GROUP BY s.state ORDER by s.state';
 	}
-	$DBRESULT = $dbb->query($rq2);
 
-	$svc_stat = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
-	$info = array();
-	$color = array();
-	$legend = array();
+	$dbResult = $dbb->query($rq2);
+	$svc_stat = [0 =>0, 1 =>0, 2 =>0, 3 =>0, 4 =>0];
+	$info = [];
+	$color = [];
+	$legend = [];
 	$counter = 0;
-	while ($data = $DBRESULT->fetchRow()) {
-		$info[$data["state"]]['count'] = $data["count"];
-		$info[$data["state"]]['acknowledged'] = $data["acknowledged"];
-		$info[$data["state"]]['downtime'] = $data["downtime"];
-		$counter += $data["count"];
-		$legend[] = $tabSatusService[$data["state"]];
+
+	while ($data = $dbResult->fetchRow()) {
+		$info[$data['state']]['count'] = $data['count'];
+		$info[$data['state']]['acknowledged'] = $data['acknowledged'];
+		$info[$data['state']]['downtime'] = $data['downtime'];
+		$counter += $data['count'];
+		$legend[] = $tabStatusService[$data['state']];
 	}
-	$DBRESULT->free();
+
+	$dbResult->free();
 
 	/**
-	 *  create the dataset
+	 *  Create the dataset
 	 */
 	foreach ($info as $key => $value) {
-        $serviceArray[$tabSatusService[$key]]['value'] = $value['count'];
+        $serviceArray[$tabStatusService[$key]]['value'] = $value['count'];
 		$valuePercent = round($value['count'] / $counter * 100, 2);
-	  	$valuePercent = str_replace(",", ".", $valuePercent);
-        $serviceArray[$tabSatusService[$key]]['percent'] = $valuePercent;
-        $serviceArray[$tabSatusService[$key]]['acknowledged'] = $value['acknowledged'];
-        $serviceArray[$tabSatusService[$key]]['downtime'] = $value['downtime'];
+	  	$valuePercent = str_replace(',', '.', $valuePercent);
+        $serviceArray[$tabStatusService[$key]]['percent'] = $valuePercent;
+        $serviceArray[$tabStatusService[$key]]['acknowledged'] = $value['acknowledged'];
+        $serviceArray[$tabStatusService[$key]]['downtime'] = $value['downtime'];
  	}
- 
-    $template->assign("services", $serviceArray);
-    
+
+    $template->assign('services', $serviceArray);
+
     /**
-	 * Display Templates
+	 * Display Template
 	 */
-	$template->display("global_health_service.ihtml");
+	$template->display('global_health_service.ihtml');
 }
